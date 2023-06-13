6c0b60fc6c2798ea3f6a296bc3c585a57508f81e
ahmednoorzakaria <ahmednoor.zakaria@student.moringaschool.com>
2023-06-13 15:38:01 +0300

    Finished Arrow functions

---
+++ index.js
@@ -0,0 +1,12 @@
+const divide = function(num1, num2) {
+    num1 = 2000;
+    num2 = 100;
+    return num1 / num2;
+  };  
+console.log(divide())
+
+const square = num => num*num ;
+square(2);
+
+const add = (num1 , num2) => num1+num2;
+add(3+4);
